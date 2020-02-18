5ccba6ee29561d872ab18f4880352a550a0da0ff
jirwincline <jci@pdx.edu>
2020-02-17 17:13:03 -0800

    successfully trims index.css/js and delete unused files

--- trim-react-app/helloworld/__main__.py
+++ trim-react-app/helloworld/__main__.py
@@ -22,21 +22,49 @@ def main():
     serviceWorkerjs = srcDest + r'\\serviceWorker.js'
     # Block where we do things
     if(path.isfile(Appjs) and args == 'trim'):
+        os.remove(Appcss)
+        os.remove(Apptest)
+        os.remove(logosvg)
+        os.remove(serviceWorkerjs)
         length = len(args)
+        # ---------------Trim App.js------------------
         with open(Appjs, 'r') as f:
             lines = f.readlines()
             for i, line in enumerate(lines):
                 if(line == "import logo from './logo.svg';\n"):
                     lines[i] = '//removed\n'
+                if(line == "import './App.css';\n"):
+                    lines[i] = '//removed\n'
+                if(line == '      <header className="App-header">\n'):
+                    for x in range(14):
+                        lines[i + x] = ''
+                    lines[i] = '      <h1>Trimmed React App</h1>\n'
+
         with open(Appjs, 'w') as trimmed:
             trimmed.writelines(lines)
-            # -----------------------------
+        # ----------------Trim index.css-----------------
+        with open(indexcss, 'r') as f:
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                if(line == "  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',\n"):
+                    for x in range(10):
+                        lines[i + x] = ''
+        with open(indexcss, 'w') as trimmed:
+            trimmed.writelines(lines)
+        # -----------------Trim index.js--------------------
+        with open(indexjs, 'r') as f:
+            lines = f.readlines()
+            lines[4] = ''
+            lines[11] = ''
+        with open(indexjs, 'w') as trimmed:
+            trimmed.writelines(lines)
     else:
         if not (args == 'trim'):
             logging.warning('\n Valid arguments:')
             print('\ntrim')
         else:
             print('incorrect directory')
+
 
     # my_function('hello world')
     # my_object = MyClass('Thomas')
